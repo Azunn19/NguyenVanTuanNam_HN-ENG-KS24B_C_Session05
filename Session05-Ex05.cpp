5778dd46ea2756b8be080281072f0d3dbe79de8b
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-25 21:14:07 +0700

    Upload bài tập session05-Ex05

---
+++ Session05-Ex05.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	
+    for(int a = 1; a <= 9; a++){
+        printf("Bang cuu chuong cua %d:\n", a);
+        for(int b = 1; b <= 10; b++){
+            printf("%d x %d = %d\n", a, b, a*b);
+        }
+    }
+
+    return 0;
+}
+
