fa25c72f5b69b0a9f8b0049ccea9b1ee5033e5e5
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-25 21:15:05 +0700

    Upload bài tập session05-Ex08

---
+++ Session05-Ex08.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int a, b, temp;
+	printf("Hay nhap 2 so nguyen duong: ");
+	scanf("%d %d", &a, &b);
+	int so_1 = a;
+	int so_2 = b;
+	while (b != 0){
+		temp = b;
+		b = a%b;
+		a = temp;
+	}
+	if(so_1 > 0 && so_2 > 0){
+		int BCNN = so_1*so_2/a; 
+		printf("Boi chung nho nhat cua %d va %d la %d\n", so_1, so_2, BCNN);
+	}
+	else{
+		printf("Du lieu nhap vao khong hop le");
+	}
+	
+	return 0;
+}
